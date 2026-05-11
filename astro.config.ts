5e0ef876a10ba78f04a15ce40b782231e2da363f
wyuser <tea_106@outlook.com>
2026-05-11 11:21:29 +0800

    0.7.4

--- astro.config.ts
+++ astro.config.ts
@@ -10,11 +10,24 @@ import {
 } from "@shikijs/transformers";
 import { transformerFileName } from "./src/utils/transformers/fileName";
 
-const siteUrl = import.meta.env.PUBLIC_SITE_URL || "https://example.com";
+//const siteUrl = import.meta.env.PUBLIC_SITE_URL || "https://example.com";
+
+// 默认设为你的 VPS 网址
+let siteUrl = 'https://martianw.cn';
+
+// 如果检测到是在 GitHub Actions 中运行构建，自动切换为 GitHub Pages 网址
+if (process.env.GITHUB_ACTIONS) {
+  siteUrl = 'https://martian2035-dev.github.io';
+}
 
 export default defineConfig({
   site: siteUrl,
-  base: "/",
+  // 如果你的 github pages 有子路径（比如 /my-repo/），还需要动态配置 base
+  // base: process.env.GITHUB_ACTIONS ? '/my-repo' : '/',
+
+// export default defineConfig({
+ // site: siteUrl,
+  // base: "/",
   integrations: [
     sitemap({
       filter: () => true,
